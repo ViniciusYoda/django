5ea1d158d4ce87cce64ec312ea037987d6db54d0
Vinícius Yoda <vyoda4604@gmail.com>
2025-05-09 10:20:05 -0300

    topics topic

--- aula1/learning_log/learning_logs/views.py
+++ aula1/learning_log/learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Página principal do Learning_log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Página que mostra todos os tópicos"""
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topic}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único tópico e todas as suas entradas"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
