bb99b9be6824dcb7c9e6dfc602c17fa519c8a4ad
Vinícius Yoda <vyoda4604@gmail.com>
2025-05-13 19:21:18 -0300

    logout

--- aula1/learning_log/users/views.py
+++ aula1/learning_log/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
-# Create your views here.
+def logout_view(request):
+    """Faz logout do usuário e redireciona para a página inicial."""
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
